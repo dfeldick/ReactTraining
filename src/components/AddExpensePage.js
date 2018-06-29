5053a1f54bb3b154648a25164b1028133f095cc5
dfeldick <40580732+dfeldick@users.noreply.github.com>
2018-06-29 07:37:54 -0700

    xsssf
    s

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddExpensePage = () => (
+  <div>
+    this is my add
+  </div>
+)
+
+export default AddExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+  <div>
+    this is my help
+  </div>
+)
+
+export default HelpPage;
